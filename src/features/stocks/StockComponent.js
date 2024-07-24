166b89fbfb7366439f131d2434aaeb37f850e7e7
unknown <bs7720405@gmail.com>
2024-07-24 22:12:13 +0300

    second commit add css

--- src/features/stocks/StockComponent.js
+++ src/features/stocks/StockComponent.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { StockLine } from './StockLine';
 import { initStocks } from './stocksSlice';
@@ -8,20 +8,63 @@ export function StockComponent() {
     const stocks = useSelector((state) => state.stocks.dataStocks);
     const dispatch = useDispatch()
 
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         const response = await fetch('localhost:5000/api/getAllStocks');
-    //         const data = await response.json();
-    //         dispatch(initStocks(data));
-    //     }
-    //     fetchData();
-    // }, []);
+    const [selectedStock, setSelectedStock] = useState(null);
+
+    const handleStockClick = (stockSymbol) => {
+        setSelectedStock(stockSymbol);
+    };
+    
 
     return (
         <div>
-            {stocks.map((item) => {
-                return <StockLine line={item} />
-            })}
+            <h1>Stocks</h1>
+            <hr style={{ border: '1px solid lightblue', width: '80%', margin: '0px auto' }} />
+
+            {Object.keys(stocks).map((stockSymbol) => (
+                <div key={stockSymbol} style={{
+                    margin: 'auto', width: '80%'
+                }}>
+                    <div
+                        key={stockSymbol}
+                        style={{
+                            paddingTop: '5px',
+                            paddingBottom: '5px',
+                            // width: '80%',
+                            // margin: '0 auto',
+                            // cursor: 'pointer',
+                            backgroundColor: selectedStock === stockSymbol ? 'red' : 'white',
+                            color: selectedStock === stockSymbol ? 'white' : 'black',
+
+                        }}
+                        onClick={() => handleStockClick(stockSymbol)}
+                    >
+                        {stockSymbol}
+                    </div>
+
+                    {selectedStock === stockSymbol && (
+                        <table style={{ margin: "0px auto", borderCollapse: 'collapse', border: '2px solid #ccc', width: '80%', borderRadius: '5px' }}>
+                        <thead>
+                          <tr>
+                            {Object.keys(stocks[stockSymbol][0]).map((record) => (
+                              <th style={{ padding: '12px', textAlign: 'left', backgroundColor: '#f2f2f2', border: '1px solid #ccc' }}>{record}</th>
+                            ))}
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {stocks[stockSymbol].map((stockRecord, index) => (
+                            <tr style={{ borderBottom: '1px solid #ccc' }}>
+                              {Object.keys(stockRecord).map((record) => (
+                                <td style={{ padding: '12px', textAlign: 'left', border: '1px solid #ccc' }}>{stockRecord[record]}</td>
+                              ))}
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+
+                    )}
+                    <hr style={{ border: '1px solid lightblue', margin: '0px', }} />
+                </div>
+            ))}
         </div>
-    )
+    );
 }
